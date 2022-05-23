7b3cc64ba346b242a4b96b5caf4daad1c51f3ff4
Harrison Sabean <hsabes@gmail.com>
2022-05-23 14:21:04 -0700

    commit tree 39e8296bb1ea3da9933ac8e383f0906ed94b32ae

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+const removed = document.getElementById('main');
+removed.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Erik is the champion';
